5fad82a0150da685dc79e906da5346246df3fa3c
Ole D <hi@cloudybyte.net>
2022-09-18 14:48:54 +0200

    feat: redirect usages and creation date
    migration updates are fine till first release

--- server/migrations/000002_create_redirects_table.up.sql
+++ server/migrations/000002_create_redirects_table.up.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS redirects(
+CREATE TABLE IF NOT EXISTS redirects (
     id TEXT PRIMARY KEY,
     redirect_to TEXT NOT NULL,
-    owner UUID NOT NULL
+    owner UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+    uses INTEGER NOT NULL DEFAULT 0
 );
