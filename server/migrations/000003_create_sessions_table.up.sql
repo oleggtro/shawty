543e6f69cd5d7de62e458c0aa908ab6793d65e86
Ole D <hi@cloudybyte.net>
2022-09-19 22:45:51 +0200

    fix: allow sessions for same user, not same token

--- server/migrations/000003_create_sessions_table.up.sql
+++ server/migrations/000003_create_sessions_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS sessions (
-    token UUID NOT NULL DEFAULT gen_random_uuid(),
-    subject UUID UNIQUE NOT NULL,
+    token UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    subject UUID NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp + (5 * interval '1 minute')
 );
