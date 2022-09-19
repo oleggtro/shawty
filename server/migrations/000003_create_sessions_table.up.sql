cfca66d386b3163d7526a9c7a74f6aebeaefa541
Ole D <hi@cloudybyte.net>
2022-09-19 22:43:29 +0200

    feat: allow multiple sessions

--- server/migrations/000003_create_sessions_table.up.sql
+++ server/migrations/000003_create_sessions_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (
-    token UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    token UUID NOT NULL DEFAULT gen_random_uuid(),
     subject UUID UNIQUE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp + (5 * interval '1 minute')
