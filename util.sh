03e574f9201801cb52ba4b5bc47986fced104058
Ole D <hi@cloudybyte.net>
2022-09-26 21:49:36 +0200

    chore: add docker image pushing to util script

--- util.sh
+++ util.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-command=$(gum choose "run server" "build server")
+command=$(gum choose "run server" "build server" "push docker image")
 
 case $command in
   "run server")
 	  go run server/main.go
 	  ;;
 	"build server")
-		tag=$(gum input --placeholder "tag")
-		docker build -t ghcr.io/cloudybyte/shawty:latest -t ghcr.io/cloudybyte/shawty:$tag -f server/Dockerfile .
-		;;
+	  tag=$(gum input --placeholder "tag")
+	  docker build -t ghcr.io/cloudybyte/shawty:latest -t ghcr.io/cloudybyte/shawty:$tag -f server/Dockerfile .
+	  ;;
+	"push docker image")
+	  docker push ghcr.io/cloudybyte/shawty
+	  ;;
 esac
